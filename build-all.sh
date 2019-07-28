42e368b3fa91db099c0329d5f46b398a4b76a4d2
Devon Hakel-Kinko <pydo@devon.ninja>
2019-07-28 18:50:45 -0400

    Added build script to build publish all docker images

---
+++ build-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for subproject in $(find . -name 'Dockerfile' | sed 's|./||')
+do
+    image_name=$(echo $subproject | awk '{split($0,a,"/"); print a[1]}')
+    (cd $image_name && docker build -t pydo/"$image_name" .)
+    echo "Building $subproject complete." 
+
+    docker push pydo/"$image_name"
+    echo "Publishing $subproject complete to hub.docker.com complete." 
+done
